294cf3c3ee5e6908c633786748d7d15a107bb7af
PramodJana <Pramodjana395@gmail.com>
2019-06-11 22:30:35 +0530

    javascript commit #4

--- js/main.js
+++ js/main.js
@@ -1,24 +1,5 @@
 
-var isPremiumUser= false;
-
-if(isPremiumUser== true)
+for(var i=0;i<10;i++)
 {
-  alert("Thanks you are a loyal customer");
-}
-else {
-  alert("You should subscribe to our premium account");
-}
-
-
-var myAge =21;
-
-if(myAge<6)
-{
-  alert("you are a child");
-}
-else if (myAge>6 && myAge<19) {
-  alert("You are a teenager");
-}
-else {
-  alert("You are an adult");
+   console.log("The value of i is" + i);
 }
