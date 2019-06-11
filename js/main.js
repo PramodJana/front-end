dca319530819a0f23fb0afe8f14c519ed298fc38
PramodJana <Pramodjana395@gmail.com>
2019-06-11 22:56:38 +0530

    javascript commit #5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+// function myfunction()
+// {
+//   alert("My name is Pramod Jana");
+// }
+//
+// myfunction();
 
-for(var i=0;i<10;i++)
+function printName(name,age=21)
 {
-   console.log("The value of i is" + i);
+  console.log("Name is",name,"Age is",age);
 }
+
+printName("Pramod Jana");
+printName("Sia",20);
