63be309b13c321522329e9123163dc25120b228e
PramodJana <Pramodjana395@gmail.com>
2019-06-23 15:27:25 +0530

    Array pt#1

--- js/main.js
+++ js/main.js
@@ -7,4 +7,12 @@ function buttonClicked()
   document.getElementById("text").innerHTML="Dont do it";
 }
 
+var hobbies=["Gaming","Travelling","Listening to kavi sammelan","coc","programmimg","music"];
+
+console.log("I no longer like my hobbie ",hobbies.pop());
+
+hobbies.push("flirting");
+
+hobbies.push("archiry");
+
 btn.addEventListener("click", buttonClicked);
